bd3f5260b7ef09186cf7e54d4b542f1b3d10b4d1
vincecyriac <vincecyriac@outlook.com>
2023-01-04 17:58:38 +0530

    UPDATED ANGULAR FOLDER STRUCTURE

--- src/front-end/ui-angular/README.md
+++ src/front-end/ui-angular/README.md
@@ -1,4 +1,4 @@
-# FrontEnd
+# UiAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.0.
 
--- src/front-end/ui-angular/src/app/app.component.spec.ts
+++ src/front-end/ui-angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front-end'`, () => {
+  it(`should have as title 'ui-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('front-end');
+    expect(app.title).toEqual('ui-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('front-end app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ui-angular app is running!');
   });
 });
